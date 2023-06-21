213475d74575473182ee5c8241a418711c9b6348
Jesse Snyder <jessetsnyder@gmail.com>
2023-06-20 18:42:20 -0600

    monorepo structure

---
+++ packages/faucet/src/chains.ts
@@ -0,0 +1,16 @@
+import { Chain } from 'wagmi'
+
+export const astria = {
+  id: 912_559,
+  name: 'Astria',
+  network: 'astria',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'Ethereum',
+    symbol: 'ETH',
+  },
+  rpcUrls: {
+    public: { http: ['http://executor.astria.localdev.me'] },
+    default: { http: ['http://executor.astria.localdev.me'] },
+  },
+} as const satisfies Chain
