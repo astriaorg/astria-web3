3553c6ae2b44dd0b3971e55cf649eec8e155dd35
Sam Bukowski <sam.c.bukowski@gmail.com>
2023-07-06 16:16:41 -0600

    added env variables to dynamically control the number of transactions and addresses

--- packages/evm-test-data/script/DeployAndCallERC20.sol
+++ packages/evm-test-data/script/DeployAndCallERC20.sol
@@ -7,14 +7,29 @@ import "../src/SolmateERC20.sol";
 
 contract DeployAndCallERC20 is Script {
     function run() public {
-        address dropAddress = address(12);
-        uint256 quantity = 50000;
+        // load loopCount from environment
+        int256 envLoopCount = vm.envInt(("LOOP_COUNT"));
+        require(envLoopCount > 0, "LOOP_COUNT must be greater than 0");
+        uint256 loopCount = uint256(envLoopCount);
+
+        // load addressCount from environment
+        int256 envAddressCount = vm.envInt(("ADDRESS_COUNT"));
+        require(envAddressCount > 0, "ADDRESS_COUNT must be greater than 0");
+        uint256 addressCount = uint256(envAddressCount);
+
+        // dynamically create address array
+        address[] memory addressArray = new address[](addressCount);
+        for (uint256 i = 0; i < addressCount; i++) {
+            addressArray[i] = address(uint160(12 + i));
+        }
+
+        uint256 quantity = 500000;
 
         vm.startBroadcast();
         SolmateERC20 drop = new SolmateERC20();
 
-        for (uint256 i = 0; i < 66; i++) {
-            drop.mint(dropAddress, quantity);
+        for (uint256 i = 0; i < loopCount; i++) {
+            drop.mint(addressArray[i % addressCount], quantity);
         }
 
         vm.stopBroadcast();
