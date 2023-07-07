eb05cbe103dfcd8b30c812914140f3e40506263f
Sam Bukowski <sam.c.bukowski@gmail.com>
2023-07-07 10:38:02 -0600

    token transfer loop added

--- packages/evm-test-data/script/DeployAndCallERC20.sol
+++ packages/evm-test-data/script/DeployAndCallERC20.sol
@@ -23,13 +23,31 @@ contract DeployAndCallERC20 is Script {
             addressArray[i] = address(uint160(12 + i));
         }
 
-        uint256 quantity = 500000;
+        uint256 quantity = 100_000_000_000_000_000_000; // 100 ETH?
 
         vm.startBroadcast();
-        SolmateERC20 drop = new SolmateERC20();
+        SolmateERC20 token = new SolmateERC20();
 
+        // mint tokens to addresses
         for (uint256 i = 0; i < loopCount; i++) {
-            drop.mint(addressArray[i % addressCount], quantity);
+            token.mint(addressArray[i % addressCount], quantity);
+        }
+
+        // transfer tokens between addresses
+        uint256 amount = 1; // 0.000001 ETH?
+        for (uint256 i = 0; i < loopCount; i++) {
+            require(
+                token.allowance(
+                    address(this),
+                    addressArray[i % addressCount]
+                ) >= amount,
+                "Transfer amount exceeds allowance"
+            );
+            token.transferFrom(
+                addressArray[i % addressCount],
+                addressArray[(i + 1) % addressCount],
+                amount
+            );
         }
 
         vm.stopBroadcast();
