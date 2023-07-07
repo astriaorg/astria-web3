ab409e52919a1eb8ecb69d9627f7cc74b3accb1d
Jesse Snyder <jessetsnyder@gmail.com>
2023-07-07 13:52:33 -0600

    gotta pay the troll toll!

--- packages/evm-test-data/script/DeployAndCallERC20.s.sol
+++ packages/evm-test-data/script/DeployAndCallERC20.s.sol
@@ -6,6 +6,9 @@ import {Script} from "forge-std/Script.sol";
 import "../src/SolmateERC20.sol";
 
 contract DeployAndCallERC20 is Script {
+    uint256 constant private QUANTITY_TO_MINT = 100 ether;
+    uint256 constant private QUANTITY_TO_TRANSFER = 1 ether;
+
     function run() public {
         // load loopCount from environment
         int256 envLoopCount = vm.envInt(("LOOP_COUNT"));
@@ -25,27 +28,22 @@ contract DeployAndCallERC20 is Script {
             addressArray[i] = payable(addr);
         }
 
-        uint256 quantity = 100 ether;
-
-        vm.startBroadcast();
+        uint256 privateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(privateKey);
+        // deploy token
         SolmateERC20 token = new SolmateERC20();
-
-        // mint tokens to addresses
-        for (uint256 i = 0; i < loopCount; i++) {
-            token.mint(addressArray[i % addressCount], quantity);
+        for (uint256 i = 0; i < addressCount; i++) {
+            // mint tokens to addresses
+            token.mint(addressArray[i], QUANTITY_TO_MINT);
+            // send some ether to addresses so they can pay for gas
+            addressArray[i].send(0.5 ether);
         }
         vm.stopBroadcast();
 
         // transfer tokens between addresses
-        uint256 amount = 1 ether;
         for (uint256 i = 0; i < loopCount; i++) {
             vm.startBroadcast(addressArray[i % addressCount]);
-            token.approve(addressArray[i % addressCount], amount);
-            token.transferFrom(
-                addressArray[i % addressCount],
-                addressArray[(i + 1) % addressCount],
-                amount
-            );
+            token.transfer(addressArray[(i + 1) % addressCount], QUANTITY_TO_TRANSFER);
             vm.stopBroadcast();
         }
 
