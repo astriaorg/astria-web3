40a95eb771691d0fd841aa8d6e6d2a45a7b71268
Sam Bukowski <sam.c.bukowski@gmail.com>
2023-07-07 14:35:05 -0600

    readme typo and went from send to call

--- packages/evm-test-data/script/DeployAndCallERC20.s.sol
+++ packages/evm-test-data/script/DeployAndCallERC20.s.sol
@@ -6,8 +6,8 @@ import {Script} from "forge-std/Script.sol";
 import "../src/SolmateERC20.sol";
 
 contract DeployAndCallERC20 is Script {
-    uint256 constant private QUANTITY_TO_MINT = 100 ether;
-    uint256 constant private QUANTITY_TO_TRANSFER = 1 ether;
+    uint256 private constant _QUANTITY_TO_MINT = 100 ether;
+    uint256 private constant _QUANTITY_TO_TRANSFER = 1 ether;
 
     function run() public {
         // load loopCount from environment
@@ -21,31 +21,37 @@ contract DeployAndCallERC20 is Script {
         uint256 addressCount = uint256(envAddressCount);
 
         // dynamically create address array
-        address payable[] memory addressArray = new address payable[](addressCount);
+        address payable[] memory addressArray = new address payable[](
+            addressCount
+        );
         for (uint256 i = 0; i < addressCount; i++) {
-            string memory mnemonic = "test test test test test test test test test test test junk";
-            (address addr,) = deriveRememberKey(mnemonic, uint32(i));
+            string
+                memory mnemonic = "test test test test test test test test test test test junk";
+            (address addr, ) = deriveRememberKey(mnemonic, uint32(i));
             addressArray[i] = payable(addr);
         }
 
+        // deploy token
         uint256 privateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(privateKey);
-        // deploy token
         SolmateERC20 token = new SolmateERC20();
         for (uint256 i = 0; i < addressCount; i++) {
             // mint tokens to addresses
-            token.mint(addressArray[i], QUANTITY_TO_MINT);
+            token.mint(addressArray[i], _QUANTITY_TO_MINT);
             // send some ether to addresses so they can pay for gas
-            addressArray[i].send(0.5 ether);
+            (bool success, ) = addressArray[i].call{value: 0.5 ether}("");
+            require(success, "Transfer failed.");
         }
         vm.stopBroadcast();
 
         // transfer tokens between addresses
         for (uint256 i = 0; i < loopCount; i++) {
             vm.startBroadcast(addressArray[i % addressCount]);
-            token.transfer(addressArray[(i + 1) % addressCount], QUANTITY_TO_TRANSFER);
+            token.transfer(
+                addressArray[(i + 1) % addressCount],
+                _QUANTITY_TO_TRANSFER
+            );
             vm.stopBroadcast();
         }
-
     }
 }
